400fd441b90fc2017abc75155e5c9d69ebea7242
Akinrinola Muiz Ayomide <125732933+MuizAyomide@users.noreply.github.com>
2024-01-28 00:26:07 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+const card1 = document.querySelector('#card1');
+const card2 = document.querySelector('#card2');
+const card3 = document.querySelector('#card3');
+const card4 = document.querySelector('#card4');
+const card5 = document.querySelector('#card5');
+const card6 = document.querySelector('#card6');
+const card7 = document.querySelector('#card7');
+const card8 = document.querySelector('#card8');
+const card9 = document.querySelector('#card9');
+const card10 = document.querySelector('#card10');
+const card11 = document.querySelector('#card11');
+const card12 = document.querySelector('#card12');
+
+
+
+                    card1.addEventListener('click', () => {
+                    var audio = new Audio("audio/dog.wav");
+                    audio.play();
+                    });
+
+                    card2.addEventListener('click', () => {
+                    var audio = new Audio("audio/mouse.mp3");
+                    audio.play();
+                    });
+
+                    card3.addEventListener('click', () => {
+                    var audio = new Audio("audio/monkey.mp3");
+                    audio.play();
+                    });
+
+                    card4.addEventListener('click', () => {
+                    var audio = new Audio("audio/whale.mp3");
+                    audio.play();
+                    });
+
+                    card5.addEventListener('click', () => {
+                    var audio = new Audio("audio/owl.mp3");
+                    audio.play();
+                    });
+
+                    card6.addEventListener('click', () => {
+                    var audio = new Audio("audio/cat.mp3");
+                    audio.play();
+                    });
+
+                    card7.addEventListener('click', () => {
+                    var audio = new Audio("audio/lion.mp3");
+                    audio.play();
+                    });
+
+                    card8.addEventListener('click', () => {
+                    var audio = new Audio("audio/sheep.mp3");
+                    audio.play();
+                    });
+
+                    card9.addEventListener('click',  () => {
+                        var audio = new Audio("audio/birds.mp3");
+                        audio.play(2000);
+                        });
+
+                    card10.addEventListener('click', () => {
+                    var audio = new Audio("audio/wolf.mp3");
+                    audio.play();
+                    });
+
+                    card11.addEventListener('click', () => {
+                    var audio = new Audio("audio/elefant.mp3");
+                    audio.play();
+                    });
+
+                    card12.addEventListener('click', () => {
+                        var audio = new Audio("audio/horse.mp3");
+                        audio.play();
+                        });              
+                  
